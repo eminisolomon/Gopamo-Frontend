7398bef697d2c91eb0f6aa8266d00d889135a512
Solomon Olatunji <realsolomon01@outlook.com>
2023-07-15 11:56:33 +0100

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { UsersComponent } from './dashboard/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    NotfoundComponent,
+    HomeComponent,
+    AccountComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
